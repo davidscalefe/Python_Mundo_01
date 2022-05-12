c6eea79eb3f0df446b07aba5caef92cf433a450c
daviscalefe <72842485+davidscalefe@users.noreply.github.com>
2022-05-11 23:08:06 -0300

    Condiçoes em Python Correção desafio 032

--- CondicoesEmPython.py
+++ CondicoesEmPython.py
@@ -54,8 +54,7 @@ else:
 # Faça um program que leia um ano qualquer e mostre se ele é BISSEXTO.
 print('==============================================================================')
 ano = int(input('Digite um ano qualquer: '))
-bi = ano % 4
-if bi == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Ano bissexto')
 else:
     print('Nao e bissexto')
